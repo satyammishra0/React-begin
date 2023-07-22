ca8b526496bcef5d83c9fd69eddf3e8737c77fdf
satyam mishra <satyamjay007@gmail.com>
2023-07-22 15:56:42 +0530

    styling component and including new files added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import Appi from "./App";
 
-ReactDOM.render(<Appi />, document.getElementById("root"));
+// ReactDOM.render(<Appi />, document.getElementById("root"));
+
+createRoot(document.getElementById("root")).render(<Appi />);
+
+// <a href="http://wordpress.zcube.in/newpress/?cat=1">See All Posts <i class="fal fa-long-arrow-right"></i></a>
