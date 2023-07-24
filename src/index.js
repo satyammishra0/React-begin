df212c78ded82a95ecfa3771d14533a353dc7cb6
satyam mishra <satyamjay007@gmail.com>
2023-07-24 09:33:29 +0530

    all files edited

--- src/index.js
+++ src/index.js
@@ -5,5 +5,3 @@ import Appi from "./App";
 // ReactDOM.render(<Appi />, document.getElementById("root"));
 
 createRoot(document.getElementById("root")).render(<Appi />);
-
-// <a href="http://wordpress.zcube.in/newpress/?cat=1">See All Posts <i class="fal fa-long-arrow-right"></i></a>
