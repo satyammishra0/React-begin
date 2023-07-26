831e741a4cbd65337e40843e7b5f43ec5ad37ef2
satyam mishra <satyamjay007@gmail.com>
2023-07-26 17:40:10 +0530

    remove custom style and added tailwind

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,49 +3,62 @@ import React from "react";
 const NavBar = () => {
   return (
     <header
-      className="header sticky-header width-100 utility_flex-center-between sticky-header"
+      className="bg-indigo-700 flex justify-between items-center px-16 py-4"
       id="main_website_header"
     >
       <div className="header-logo">
-        <h1>Dummy Logo</h1>
+        <h1 className="text-2xl font-bold text-slate-100">Dummy Logo</h1>
       </div>
-      <nav>
-        <ul className="nav-list utility_flex utility_flex-center-center">
-          <li className="nav-links">
-            <a href="#"> HOME</a>
-          </li>
-          <li className="nav-links">
-            <a href="#"> ABOUT</a>
-          </li>
-          <li className="nav-links">
-            <a href="#"> PROJECTS</a>
-            <div className="sub-menu sub-menu-car" id="sub_menu">
-              <div className="sub-menu-child w-100 ">
-                <ul className="w-100 py-2">
-                  <li className="w-100">
-                    <a className="w-100" href="#">
-                      Project Aevum
-                    </a>
-                  </li>
-                  <li className="w-100">
-                    <a className="w-100" href="#">
-                      Project Aria
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
-          <li className="nav-links">
-            <a href="#"> AMENITIES</a>
-          </li>
-          <li className="nav-links">
-            <a href="#"> GALLERY</a>
-          </li>
-          <li className="nav-links">
-            <a href="#"> CONTACT</a>
-          </li>
-          <li className="nav-links">
+      <ul className="flex items-center gap-x-3 ">
+        <li className="nav-links">
+          <a
+            href="#"
+            className="text-slate-100 font-normal hover:text-indigo-300 transiiton-all ease duration-300"
+          >
+            HOME
+          </a>
+        </li>
+        <li className="nav-links">
+          <a
+            href="#"
+            className="text-slate-100 font-normal hover:text-indigo-300 transiiton-all ease duration-300"
+          >
+            ABOUT
+          </a>
+        </li>
+        <li className="nav-links">
+          <a
+            href="#"
+            className="text-slate-100 font-normal hover:text-indigo-300 transiiton-all ease duration-300"
+          >
+            PROJECTS
+          </a>
+        </li>
+        <li className="nav-links">
+          <a
+            href="#"
+            className="text-slate-100 font-normal hover:text-indigo-300 transiiton-all ease duration-300"
+          >
+            AMENITIES
+          </a>
+        </li>
+        <li className="nav-links">
+          <a
+            href="#"
+            className="text-slate-100 font-normal hover:text-indigo-300 transiiton-all ease duration-300"
+          >
+            GALLERY
+          </a>
+        </li>
+        <li className="nav-links">
+          <a
+            href="#"
+            className="text-slate-100 font-normal hover:text-indigo-300 transiiton-all ease duration-300"
+          >
+            CONTACT
+          </a>
+        </li>
+        {/* <li className="nav-links">
             <a href="https://www.instagram.com/futeragroup/">
               <i className="bx bxl-instagram"></i>
             </a>
@@ -55,24 +68,8 @@ const NavBar = () => {
             <a href="https://www.youtube.com/@Futeragroup">
               <i className="bx bxl-youtube"></i>
             </a>
-          </li>
-        </ul>
-      </nav>
-      {/* <!-- --------------------- -->
-    <!-- Mobile nav styling here -->
-    <!-- --------------------- --> */}
-      <div className="mobile-navbar-btn">
-        <i
-          className="bx bx-menu mobile-nav-icon"
-          id="menu-icon"
-          name="menu-sharp"
-        ></i>
-        <i
-          className="bx bx-home mobile-nav-icon"
-          id="close-icon"
-          name="close-sharp"
-        ></i>
-      </div>
+          </li> */}
+      </ul>
     </header>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./header.css";
-import "./utility.css";
-import "./global.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
