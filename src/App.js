e37901bd4b79a8d21810c1a4b75e5eecb5dcc646
satyam mishra <satyamjay007@gmail.com>
2023-07-27 13:10:55 +0530

    card component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import NavBar from "./Navbar";
 import Footer from "./footer";
 import HeroSection from "./heroSection";
+import cardimg1 from "./card-1.jpg";
+import cardimg2 from "./card-2.jpg";
+import cardimg3 from "./card-3.jpg";
+import CardsSection from "./cards-section";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <HeroSection />
+      <CardsSection />
       <Footer />
     </div>
   );
